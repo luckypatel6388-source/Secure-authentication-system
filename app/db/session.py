9b1740eb89037916379ed95d1e9d1b0053b470ab
Lucky Patel <luckypatel6388@gmail.com>
2026-07-30 21:36:01 +0530

    done

--- app/db/session.py
+++ app/db/session.py
@@ -2,6 +2,7 @@ from typing import AsyncGenerator
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase
 from app.core.config import settings
+import asyncpg
 
 # Create Async Engine for MySQL
 engine = create_async_engine(
